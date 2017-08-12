2ac3e500e5301a8d784b19d413783947f757f9b2
Matt McCormick <matt@mbmccormick.com>
2017-08-11 21:09:17 -0700

    Adding headlines.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var Stock = require("google-stocks");
 var Moment = require("moment-timezone");
+var StockNews = require("google-finance");
 
 exports.handler = function (event, context, callback) {
     if (event.source == "aws.events") {
@@ -8,44 +9,61 @@ exports.handler = function (event, context, callback) {
         return callback(null, "Success");
     }
 
-    Stock(['MSFT'], function (error, data) {
+    Stock(['MSFT'], function (err, data) {
         var quote = data[0];
 
+        var exchange = quote.e;
+        var ticker = quote.t;
         var lastTrade = Moment.tz(quote.lt_dts.slice(0, -1), "America/New_York");
+        var price = quote.l_fix;
+        var change = quote.cp_fix.replace("+", "").replace("-", "");
 
-        var uid = quote.e + ":" + quote.t + ":" + lastTrade.toISOString();
-        var updateDate = lastTrade.toISOString();
-        var titleText = "$MSFT update for " + lastTrade.format("MMMM D, YYYY [at] h:mma");
+        StockNews.companyNews({ symbol: 'MSFT' }, function (err, data) {
+            var news = data;
 
-        var mainText;
+            var headlines = "";
+            for (var i = 0; i < news.length; i++) {
+                if (i == 3) {
+                    break;
+                }
 
-        var now = Moment();
-        if (now.diff(lastTrade, "minutes") < 5) {
-            // currently trading
-            mainText = "Microsoft Corporation stock is currently trading at $" + quote.l_fix + " per share, " + (quote.cp_fix > 0 ? "up" : "down") + " " + quote.cp_fix + " points from the previous close.";
-        }
-        else {
-            var calendarOptions = {
-                sameDay: "[today]",
-                nextDay: "[tomorrow]",
-                nextWeek: "dddd",
-                lastDay: "[yesterday]",
-                lastWeek: "[last] dddd",
-                sameElse: "[on] dddd"
-            };
+                headlines += " ... " + news[i].title;
+            }
 
-            // trading closed
-            mainText = "Microsoft Corporation stock closed at $" + quote.l_fix + " per share " + lastTrade.calendar(null, calendarOptions) + ", " + (quote.cp_fix > 0 ? "up" : "down") + " " + quote.cp_fix + " points from the previous close.";
-        }
+            var uid = exchange + ":" + ticker + ":" + lastTrade.toISOString();
+            var updateDate = lastTrade.toISOString();
+            var titleText = "$MSFT update for " + lastTrade.format("MMMM D, YYYY [at] h:mma");
 
-        var redirectionUrl = "https://www.google.com/finance?q=NASDAQ:MSFT";
+            var mainText;
 
-        callback(null, {
-            "uid": uid,
-            "updateDate": updateDate,
-            "titleText": titleText,
-            "mainText": mainText,
-            "redirectionUrl": redirectionUrl
+            var now = Moment();
+            if (now.diff(lastTrade, "minutes") < 5) {
+                // currently trading
+                mainText = "Microsoft Corporation is currently trading at $" + price + " per share, " + (change > 0 ? "up" : "down") + " " + change + " points from the previous close. Here are the latest headlines:" + headlines;
+            }
+            else {
+                var calendarOptions = {
+                    sameDay: "[today]",
+                    nextDay: "[tomorrow]",
+                    nextWeek: "dddd",
+                    lastDay: "[yesterday]",
+                    lastWeek: "[last] dddd",
+                    sameElse: "[on] dddd"
+                };
+
+                // trading closed
+                mainText = "Microsoft Corporation closed at $" + price + " per share " + lastTrade.calendar(null, calendarOptions) + ", " + (change > 0 ? "up" : "down") + " " + change + " points from the previous close. Here are the latest headlines:" + headlines;
+            }
+
+            var redirectionUrl = "https://www.google.com/finance?q=NASDAQ:MSFT";
+
+            callback(null, {
+                "uid": uid,
+                "updateDate": updateDate,
+                "titleText": titleText,
+                "mainText": mainText,
+                "redirectionUrl": redirectionUrl
+            });
         });
     });
 };
